5d6239c0f21ed3048b9176b2a0644bdeca2b76d5
Bopedig <alex11owen@hotmail.co.uk>
2020-06-10 13:02:22 +0100

    first

---
+++ build/precache-manifest.1d5e3d3d906efcee2097f166300929c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94049cbb8bb59fbceca33b36cd68e75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bbb4e0c6df9c37a4554",
+    "url": "/static/css/main.5a2d17af.chunk.css"
+  },
+  {
+    "revision": "0beaea1df6ff0a038773",
+    "url": "/static/js/2.055eda93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.055eda93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bbb4e0c6df9c37a4554",
+    "url": "/static/js/main.fb7a7987.chunk.js"
+  },
+  {
+    "revision": "914d8d011fee83d6d1a9",
+    "url": "/static/js/runtime-main.eefb9976.js"
+  }
+]);
